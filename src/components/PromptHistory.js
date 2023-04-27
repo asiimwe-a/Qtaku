532754017323e1b05c8d1fce14692794cf4b2740
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-04-27 23:56:18 +0300

    Modify code for prompt history

--- src/components/PromptHistory.js
+++ src/components/PromptHistory.js
@@ -1,7 +1,31 @@
+// import React from "react";
+
+// const PromptHistory = () => {
+//   return <div>PromptHistory</div>;
+// };
+
+// export default PromptHistory;
+
 import React from "react";
 
 const PromptHistory = () => {
-  return <div>PromptHistory</div>;
+  const promptHistory = [
+    "Generate a random number between 1 and 10",
+    "Create a recipe for a vegan lasagna",
+    "Write a short story about a haunted house",
+    // add more prompts here as desired
+  ];
+
+  return (
+    <div>
+      <h2>Prompt History</h2>
+      <ul>
+        {promptHistory.map((prompt, index) => (
+          <li key={index}>{prompt}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PromptHistory;
