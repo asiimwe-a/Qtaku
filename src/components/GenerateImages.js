87dbb79a58a7597cc1bfba7f43ef53399a7f0b4a
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-04-30 23:55:04 +0300

    Add Generate images syntax

--- src/components/GenerateImages.js
+++ src/components/GenerateImages.js
@@ -1,7 +1,37 @@
-import React from "react";
+// import React from "react";
+
+// const GenerateImages = () => {
+//   return <div>GenerateImages</div>;
+// };
+
+// export default GenerateImages;
+
+import React, { useState, useEffect } from "react";
 
 const GenerateImages = () => {
-  return <div>GenerateImages</div>;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      try {
+        const response = await fetch("your-api-endpoint");
+        const data = await response.json();
+        setImages(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchImages();
+  }, []);
+
+  return (
+    <div>
+      {images.map((image) => (
+        <img key={image.id} src={image.url} alt={image.alt} />
+      ))}
+    </div>
+  );
 };
 
 export default GenerateImages;
