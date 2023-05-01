0f4af90a0c68b61bebe39ca052297b10a063ddfb
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-05-01 22:38:16 +0300

    Create prompt code modification

--- src/components/CreatePrompt.js
+++ src/components/CreatePrompt.js
@@ -1,5 +1,20 @@
 const CreatePrompt = ({ onCreate }) => {
-  return <div>CreatePrompt</div>;
+  const [prompt, setPrompt] = useState("");
+
+  const handlePromptChange = (event) => {
+    setPrompt(event.target.value);
+  };
+
+  const handleCreateClick = () => {
+    onCreate(prompt);
+  };
+
+  return (
+    <div>
+      <input type="text" value={prompt} onChange={handlePromptChange} />
+      <button onClick={handleCreateClick}>Create</button>
+    </div>
+  );
 };
 
 export default CreatePrompt;
